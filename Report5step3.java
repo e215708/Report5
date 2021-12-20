8e201aa13e058114974a3ee68f3cdab75ab0c22f
SK <s@SKnoMacBook-Air.local>
2021-12-21 00:35:28 +0900

    1st commit

---
+++ Report5step3.java
@@ -0,0 +1,9 @@
+public class Report5step3 {
+    public static void main(String[] arg) {
+        String str = "百二十三";
+         int value = Integer.parseInt(str);
+
+         System.out.println(value);
+     }
+}
+
