66736c1b8b66d4a8cfcc2308b40f21aff10f0bca
SK <s@SKnoMacBook-Air.local>
2021-12-20 23:48:29 +0900

    1st commit

--- Report5.java
+++ Report5.java
@@ -1,6 +1,11 @@
 public class Report5{
     public static void main(String[] arg) {
+        try{
         String str = null;
         System.out.println(str.length());
+    }catch(NullPointerException e){
+        System.out.println("NullPointerExceptionが発生した");
+        System.out.println(e.getMessage());
+    }
     }
 }
